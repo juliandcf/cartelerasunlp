574d197499f7757d34e9fa7ea91735d3eeefda3f
andres <andres@andres-Aspire-V5-431>
2017-07-25 10:38:36 -0300

    publicacion modelo y DTO

--- CartelerasTTPS/src/dto/PublicacionVO.java
+++ CartelerasTTPS/src/dto/PublicacionVO.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
@@ -19,7 +21,9 @@ public class PublicacionVO extends GenericVO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	public Long id;
+	
 	public String titulo;
+	
 	public String texto;
 	@JsonFormat(shape=JsonFormat.Shape.STRING, pattern="dd-MM-yyyy hh:mm:ss")
 	public Date fecha;
